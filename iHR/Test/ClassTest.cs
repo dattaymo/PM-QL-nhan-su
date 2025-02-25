e9dd8727d802a078adda17d9cd3d5c7ec7e22c7b
dattaymo <cdatiendat@gmail.com>
2025-02-25 22:55:23 +0700

    thêm ClassTest

---
+++ iHR/Test/ClassTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iHR.Test
+{
+    internal class ClassTest
+    {
+           public void TestMenthod()
+        {
+            int i;
+            i = 10;
+            string Name = "Đạt";
+            Console.WriteLine(Name);
+        }
+    }
+}
